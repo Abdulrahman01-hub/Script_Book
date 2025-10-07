8aa5d78de219ab4bdfdbb36b13f03d22887b5c25
Abdulrahman01-hub <barbarossa.123456.1@gmail.com>
2025-10-07 12:25:28 +0400

    math task

---
+++ main.js
@@ -0,0 +1,86 @@
+// let arr = [
+//     [20, 45, 10, 15, 5],
+//     [34, 5, 10, 20, 4],
+//     [5, 220, 20, 10, 6]
+//     ]
+
+
+// let arr = [
+//     [20, 45, 10, 15, 5,],
+//     [34, 5, 10, 20, 4,],
+//     [5, 220, 20, 10, 6,]
+// ]
+// let result = arr.reduce((a, b) => a.filter(c => b.includes(c)));
+// console.log(result);
+
+// let arr = ["murad", "ali", "amir", "nilufer", "qafar", "altay"]
+// let result = [];
+
+// function callback(item) {
+//     if (item[0] === "a" || item[0] === "A") {
+//         result.push(item)
+//     }
+//     arr.forEach(callback)
+//     console.log(result)
+
+// }
+// let startWitch = "A"
+
+//?task 1
+//? let x = 3;
+//? let y;
+
+//? if (x >= 0) {
+//?     y = x ** 3 + 2 * x ** 2 + 4 * x - 6;
+//? } else {
+//?     y = x ** 3 - 7 * x;
+//? }
+//? console.log(y);
+
+//!task 2
+//! let x = 2;
+//! let y;
+
+//! if (x < 5) {
+//!     y = x ** 2 - 3 * x + 4;
+//! } else {
+//!     y = x + 7;
+//! }
+//! console.log(y);
+
+//task 3
+// let x = 20; 
+// let y;
+
+// if (x >= 10) {
+//     y = Math.pow(x, 3) + 5 * x;
+// } else {
+//     y = Math.pow(x, 2) - 2 * x + 4;
+// }
+// console.log(y);
+
+//task 4
+// let x = 3;
+// let y;
+
+// if (x < -4) {
+//     y = x + 5;
+// } else if (x <= 7) {
+//     y = x ** 2 - 3 * x;
+// } else {
+//     y = x ** 3 + 2 * x;
+// }
+
+// console.log(y);
+
+//task 5
+// let x = 8;
+// let y;
+
+// if (x >= 13) {
+//     y = 3 * x ** 3 + 4 * x ** 2 + 5 * x + 6;
+// } else {
+//     y = 3 * x ** 3 - 2 * x ** 2 - 3 * x - 4;
+// }
+
+// console.log(y);
