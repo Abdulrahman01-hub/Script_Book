c50054cda91bfc9ba2ba09b030a816b689834585
Abdulrahman01-hub <barbarossa.123456.1@gmail.com>
2025-10-08 13:14:12 +0400

    task

--- main.js
+++ main.js
@@ -38,3 +38,11 @@
 // listItems.forEach(item => {
 //     item.classList.add('item');
 // });
+
+// if (x >= 13) {
+//     y = 3 * x ** 3 + 4 * x ** 2 + 5 * x + 6;
+// } else {
+//     y = 3 * x ** 3 - 2 * x ** 2 - 3 * x - 4;
+// }
+
+// console.log(y);
